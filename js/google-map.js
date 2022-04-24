e707a803df60fc78e5a52f768793cf29dfebfd2a
Ceothegod <73771972+sashwatkoirala@users.noreply.github.com>
2022-04-24 11:04:21 +0545

    prasil gay

--- js/google-map.js
+++ js/google-map.js
@@ -5,7 +5,7 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(34.0479, 100.6197);
+    var myLatlng = new google.maps.LatLng(28.3949, 84.1240);
     // 39.399872
     // -8.224454
     
