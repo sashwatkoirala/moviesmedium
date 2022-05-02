95441d40eb34ae34fbb72f8268616ac594cb9765
Ceothegod <73771972+sashwatkoirala@users.noreply.github.com>
2022-05-02 08:18:40 +0545

    updated files

--- test/login/dashboard.php
+++ test/login/dashboard.php
@@ -50,7 +50,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "name: " . $row["movie_name"]. " <hr>" . $row["move_thumbnail"]."<br><br>";
+    echo "name: " . $row["movie_name"]. " <hr><img src='" . $row["move_thumbnail"]."' width='100px'><br><br>";
   } 
 } else {
   echo "0 results";
